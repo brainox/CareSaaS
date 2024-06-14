ce0dfe4d31fff0a649ed63a402ea07e3e87d6415
Obinna <macmartins081@gmail.com>
2024-06-15 00:19:21 +0400

    Initial Commit

---
+++ CareSaaS/CareSaaSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CareSaaSApp.swift
+//  CareSaaS
+//
+//  Created by Obinna on 15/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct CareSaaSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CareSaaS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CareSaaS
+//
+//  Created by Obinna on 15/06/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}

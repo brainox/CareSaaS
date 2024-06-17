14fe5f4e2ab7cb6853cc984c500d9faa163ff175
Obinna <macmartins081@gmail.com>
2024-06-17 16:48:33 +0400

    Renamed ContentView to RootView

--- CareSaaS/Views/RootView.swift
+++ CareSaaS/Views/RootView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  RootView.swift
 //  CareSaaS
 //
 //  Created by Obinna on 15/06/2024.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct RootView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    RootView()
 }
